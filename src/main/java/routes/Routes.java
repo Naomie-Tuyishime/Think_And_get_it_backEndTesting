33e976d3a99296e9a9192fd7cdf2f8bab57ce537
Naomie-Tuyishime <tuyishimenaome58@gmail.com>
2026-06-05 09:36:02 +0200

    changing user password

--- src/main/java/routes/Routes.java
+++ src/main/java/routes/Routes.java
@@ -11,4 +11,6 @@ public class Routes {
     public static final String GET_SINGLE_USER = "auth/me";
     public static final String REFRESH_ACCESS_TOKEN = "auth/refresh";
     public static final String  USER_PROFILE = "users/profile";
+    public static final String USER_CHANGE_PASSWORD = "users/change-password";
+
 }
