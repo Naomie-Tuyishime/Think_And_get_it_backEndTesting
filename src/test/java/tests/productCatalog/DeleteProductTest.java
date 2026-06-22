9bd82c37979a187e64f1a783f4724cff149add70
Naomie-Tuyishime <tuyishimenaome58@gmail.com>
2026-06-22 15:51:07 +0200

    improving readability by adding apiService for businessLogic

--- src/test/java/tests/productCatalog/DeleteProductTest.java
+++ src/test/java/tests/productCatalog/DeleteProductTest.java
@@ -1,35 +1,26 @@
 package tests.productCatalog;
-
-import constants.StatusCode;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-import static routes.Routes.PRODUCT;
-import static routes.Routes.fakeProductId;
-import static spec.SpecBuilder.getRequestSpec;
-import static spec.SpecBuilder.getResponseSpec;
+import static apiService.DeleteProduct.deleteProductRequest;
+import static apiService.DeleteProduct.getProductRequest;
+import static routes.Routes.*;
 import static tests.productCatalog.CreatingProduct.createProductTest;
 import static utils.TokenManager.getToken;
 
 public class DeleteProductTest {
     String token = getToken("login");
-    String targetProductId = createProductTest();
+    String targetProductId;
+    @BeforeClass
+    public void setUp() {
+ targetProductId = createProductTest();
+    }
+
     @Test
     public void deleteProductTest (){
-        Response response =
-                given()
-                        .spec(getRequestSpec())
-                        .pathParam("id", targetProductId)
-                        .header("Authorization", "Bearer " + token)
-                        .when()
-                        .delete(PRODUCT)
-                        .then().spec(getResponseSpec())
-                        .statusCode(StatusCode.CODE_200.getCode())
-                        .extract()
-                        .response();
-
+        Response response = deleteProductRequest(targetProductId, token);
         Assert.assertEquals(response.jsonPath().getString("message"), "Product deactivated");
 
         String deletedId = response.jsonPath().getString("data.id");
@@ -43,33 +34,14 @@ public class DeleteProductTest {
 
     @Test
     public void  deleteProductWithNonExistingId (){
-      Response response =  given()
-                .spec(getRequestSpec())
-                .pathParam("id",  fakeProductId)
-                .header("Authorization", "Bearer " + token)
-                .when()
-                .delete(PRODUCT)
-                .then().spec(getResponseSpec())
-                .statusCode(StatusCode.CODE_404.getCode())
-                .extract()
-                .response();
+        Response response = deleteProductRequest(FAKEPRODUCTID , token);
         Assert.assertEquals(response.jsonPath().getString("message"), "Record not found");
 
 
     }
     @Test
     public void verifyIfProductDeletedSuccessfully (){
-        Response getResponse =
-                given()
-                        .spec(getRequestSpec())
-                        .pathParam("id", targetProductId)
-                        .header("Authorization", "Bearer " + token)
-                        .when()
-                        .get(PRODUCT)
-                        .then().spec(getResponseSpec())
-                        .statusCode(StatusCode.CODE_404.getCode())
-                        .extract()
-                        .response();
+        Response getResponse = getProductRequest(targetProductId, token);
         Assert.assertEquals(getResponse.jsonPath().getString("message"), "Product not found");
 
     }
